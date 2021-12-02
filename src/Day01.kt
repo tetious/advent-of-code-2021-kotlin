2b134c3320e1692384801ec71e3afe367601162c
Greg Lincoln <greg@greglincoln.com>
2021-12-01 21:51:46 -0500

    Day 1!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
+    fun countIncreases(input: List<Int>): Int {
+        return input.mapIndexed { i, it -> if (i > 0 && it > input[i - 1]) 1 else 0 }.sum()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return countIncreases(input.map { it.toInt() })
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowed = List(input.size - 2) { i -> input.slice(i..i + 2).sumOf { it.toInt() } }
+        return countIncreases(windowed)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("day_01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("day_01")
     println(part1(input))
     println(part2(input))
 }
